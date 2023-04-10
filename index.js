753e0abe6861214eefb902ed3748dd034876f37e
harijanakondaiah <123466412+harijanakondaiah@users.noreply.github.com>
2023-04-11 04:12:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+let Element01 = document.getElementById("mano");
+
+
+
+
+
+
+
+
+var xmr = new XMLHttpRequest();
+xmr.open("GET", "https://5d76bf96515d1a0014085cf9.mockapi.io/quiz", true);
+
+
+xmr.onreadystatechange = function(){
+    if(xmr.readyState === 4 && xmr.status === 200){
+        data01 = JSON.parse(xmr.response);
+        displayquestions()
+
+    }
+
+}
+
+function displayquestions(){
+
+    let output = ""
+    for(let mano01 in data01){
+
+
+        sachin = `
+        <div class=questions>
+        <h1 class="heading02">Q${data01[mano01].id} . ${data01[mano01].question}</h1>
+        <label><input type = "radio" id = "1" name = "question${data01[mano01].id}"> ${data01[mano01].options[0]}</label><br>
+        <label><input type = "radio" id = "2" name = "question${data01[mano01].id}"> ${data01[mano01].options[1]}</label><br>
+        <label><input type = "radio" id = "3" name = "question${data01[mano01].id}"> ${data01[mano01].options[2]}</label><br>
+        <label><input type = "radio" id = "4" name = "question${data01[mano01].id}"> ${data01[mano01].options[3]}</label><br>
+        <hr class="horizontal">
+        </div>
+        `
+        output += sachin
+
+    }
+    Element01.innerHTML = output;
+
+
+    let btnElement = document.getElementById("button01");
+    let score_container = document.getElementById("score-count");
+
+    btnElement.onclick = function(){
+        let score = 0;
+        for(let i = 0; i < data01.length; i++){
+            let manohar01 = document.getElementsByName(`question${data01[i].id}`)
+            for(let j = 0; j < manohar01.length; j++){
+                if(manohar01[j].checked && manohar01[j].id == data01[i].answer){
+                    score++;
+                }
+                
+            }
+            
+        }
+        score_container.innerHTML = `${score}/5`
+        
+    }
+   
+}
+xmr.send()
